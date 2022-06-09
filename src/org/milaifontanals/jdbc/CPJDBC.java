b1bdb7ea3caf8df23527d9fb2988b9121424f952
Anna <areyes@milaifontanals.org>
2022-06-09 19:32:22 +0200

    09/06/2022

--- src/org/milaifontanals/jdbc/CPJDBC.java
+++ src/org/milaifontanals/jdbc/CPJDBC.java
@@ -12,9 +12,16 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.milaifontanals.interficie.GestioProjectesException;
 import org.milaifontanals.interficie.IGestioProjectes;
 import org.milaifontanals.model.Projecte;
@@ -28,6 +35,7 @@ import org.milaifontanals.model.Usuari;
 public class CPJDBC implements IGestioProjectes {
     
     private Connection con;
+    HashMap<String, String> hmLogin = new HashMap<String, String>();
 
     public CPJDBC() throws GestioProjectesException {
         this("CPJDBC.properties");
@@ -107,6 +115,72 @@ public class CPJDBC implements IGestioProjectes {
     }
 
     @Override
+    public String hashMD5(String input) throws GestioProjectesException {
+        try {
+  
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+  
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+  
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+  
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } 
+  
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Override
+    public String Login(String login, String password) throws GestioProjectesException {
+        
+        String token = hmLogin.get(login);
+        if (token != null){
+            return token;
+        }
+        
+        try {
+            String consulta = "select usu_id, usu_login, usu_password_hash from usuari where usu_login = ?";
+            PreparedStatement pst = con.prepareStatement(consulta);
+            pst.setString(1, login);
+            
+            ResultSet rs = pst.executeQuery();
+            
+            if (rs.next()){
+                Integer usu_id = rs.getInt("usu_id");
+                String usu_login = rs.getString("usu_login");
+                String usu_password_hash = rs.getString("usu_password_hash");
+                token = getToken();
+                hmLogin.put(login, token);
+                System.out.println("login: " + usu_login + " - contrasenya: " + usu_password_hash);
+                return token;
+            }
+            return null;
+        } catch (SQLException ex) {
+            throw new GestioProjectesException("Error en la consulta: " + ex.getMessage());
+        }
+    }
+    
+    
+    
+    
+    public String getToken() {
+        return UUID.randomUUID().toString();
+    }
+    
+    
+    @Override
     public List<Usuari> getLlistaUsuaris() throws GestioProjectesException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
@@ -185,32 +259,6 @@ public class CPJDBC implements IGestioProjectes {
     public boolean existeixProjecte(int id) throws GestioProjectesException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    @Override
-    public String hashMD5(String input) throws GestioProjectesException {
-        try {
-  
-            // Static getInstance method is called with hashing MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-  
-            // digest() method is called to calculate message digest
-            //  of an input digest() return array of byte
-            byte[] messageDigest = md.digest(input.getBytes());
-  
-            // Convert byte array into signum representation
-            BigInteger no = new BigInteger(1, messageDigest);
-  
-            // Convert message digest into hex value
-            String hashtext = no.toString(16);
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
-            }
-            return hashtext;
-        } 
-  
-        // For specifying wrong message digest algorithms
-        catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    
+    
 }
